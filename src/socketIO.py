caa1f86771d25aa0b7764c5a8784cad2ab358c21
Luis D <luis.deutsch@fh-bielefeld.de>
2018-06-07 18:06:18 +0200

    hopefully deployable

--- src/socketIO.py
+++ src/socketIO.py
@@ -22,7 +22,7 @@ class DeepGreenNamespace(BaseNamespace):
     def on_reconnect(self):
         print('reconnect')
 
-socketIO = SocketIO('localhost', 5000)
+socketIO = SocketIO('54.93.171.91', 8008)
 deepgreen_namespace = socketIO.define(DeepGreenNamespace, '/deep-green')
 deepgreen_namespace.on('move',DeepGreenNamespace.on_makeMove)
 deepgreen_namespace.emit('receive',testdata)
