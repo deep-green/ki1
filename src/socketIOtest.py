356876cd8f68e4c77d545996efc9b9101a9a3be6
Luis D <luis.deutsch@fh-bielefeld.de>
2018-06-07 18:19:33 +0200

    fix

--- src/socketIOtest.py
+++ src/socketIOtest.py
@@ -35,4 +35,4 @@ if __name__ == '__main__':
     app = socketio.Middleware(sio, app)
 
     # deploy as an eventlet WSGI server
-    eventlet.wsgi.server(eventlet.listen(('54.93.171.91', 8008)), app)
+    eventlet.wsgi.server(eventlet.listen(('', 8008)), app)
