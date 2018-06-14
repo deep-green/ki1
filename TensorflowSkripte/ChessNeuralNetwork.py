0d4a148aff96dd69e3136dd28277398df4691662
Luis D <luis.deutsch@fh-bielefeld.de>
2018-06-14 19:07:44 +0200

    working Model

--- TensorflowSkripte/ChessNeuralNetwork.py
+++ TensorflowSkripte/ChessNeuralNetwork.py
@@ -1,47 +1,64 @@
 import tensorflow as tf
 
-#HiddenLayerNodes = Root(input*output) 
-n_nodes_hl1 = 100
-n_nodes_hl2 = 100
-n_nodes_hl3 = 100
+# Parameters
+learning_rate = 0.1
+num_steps = 500
+batch_size = 128
+display_step = 100
 
-#Output
-n_classes = 148
+# Network Parameters
+n_hidden_1 = 500 # 1st layer number of neurons
+n_hidden_2 = 500 # 2nd layer number of neurons
+n_hidden_3 = 500
+num_input = 64 # Chess-Tiles
+num_classes = 4032 # Moves in Chess
 
-batch_size = 20
+# tf Graph input
+X = tf.placeholder("float", [None, num_input])
+Y = tf.placeholder("float", [None, num_classes])
 
-#Input 64
-x = tf.placeholder('float', [None, 64])
-y = tf.placeholder('float')
+# Store layers weight & bias
+weights = {
+    'h1': tf.Variable(tf.random_normal([num_input, n_hidden_1])),
+    'h2': tf.Variable(tf.random_normal([n_hidden_1, n_hidden_2])),
+    'h3': tf.Variable(tf.random_normal([n_hidden_2, n_hidden_3])),
+    'out': tf.Variable(tf.random_normal([n_hidden_3, num_classes]))
+}
+biases = {
+    'b1': tf.Variable(tf.random_normal([n_hidden_1])),
+    'b2': tf.Variable(tf.random_normal([n_hidden_2])),
+    'b3': tf.Variable(tf.random_normal([n_hidden_3])),
+    'out': tf.Variable(tf.random_normal([num_classes]))
+}
 
 
-def chess_neural_network_model(data):
-    hidden_1_layer = {'weights': tf.Variable(tf.random_normal([64, n_nodes_hl1])),
-                      'biases': tf.Variable(tf.random_normal([n_nodes_hl1]))}
+# Create model
+def neural_net(x):
+    # Hidden fully connected layer with 500 neurons
+    layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'])
+    # Hidden fully connected layer with 500 neurons
+    layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'])
+    # Hidden fully connected layer with 500 neurons
+    layer_3 = tf.add(tf.matmul(layer_2, weights['h3']), biases['b3'])
+    # Output fully connected layer with a neuron for each class
+    out_layer = tf.matmul(layer_3, weights['out']) + biases['out']
+    return out_layer
+arr = []
+for x in range(64):
+    arr.append(x)
+arrr = [arr]
+prr= []
+for y in range(4032):
+    prr.append(0)
+prrr = [prr]
+
+# Construct model
+logits = neural_net(X)
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())   
+    test = sess.run(logits, feed_dict={X: arrr, Y: prrr})
+    tesst = test[0]
+    for f in tesst:
+        print(f)
+
     
-    hidden_2_layer = {'weights': tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2])),
-                      'biases': tf.Variable(tf.random_normal([n_nodes_hl2]))}
-                      
-    hidden_3_layer = {'weights': tf.Variable(tf.random_normal([n_nodes_hl2, n_nodes_hl3])),
-                      'biases': tf.Variable(tf.random_normal([n_nodes_hl3]))}
-
-    output_layer = {'weights': tf.Variable(tf.random_normal([n_nodes_hl3, n_classes])),
-                    'biases': tf.Variable(tf.random_normal([n_classes]))}
-
-    l1 = tf.add(tf.matmul(data, hidden_1_layer['weights']) + hidden_1_layer['biases'])
-    l1 = tf.nn.relu(l1)
-
-    l2 = tf.add(tf.matmul(l1, hidden_2_layer['weights']) + hidden_2_layer['biases'])
-    l2 = tf.nn.relu(l2)
-
-    l3 = tf.add(tf.matmul(l2, hidden_3_layer['weights']) + hidden_3_layer['biases'])
-    l3 = tf.nn.relu(l3)
-
-    output = tf.add(tf.matmul(l2, hidden_3_layer['weights']) + hidden_3_layer['biases'])
-    print("Test")
-
-    return output
-
-
-test = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-chess_neural_network_model(test)
