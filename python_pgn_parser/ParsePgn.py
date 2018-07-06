4b8f523658749d9514db18d92801eb83b880a67e
Luisnator <LuisDeutsch98@gmail.com>
2018-07-06 15:31:16 +0200

    pgn-parser start

---
+++ python_pgn_parser/ParsePgn.py
@@ -0,0 +1,14 @@
+import chess
+import chess.pgn
+
+board = chess.Board()
+pgn = open("Kasparov.pgn")
+first_game = chess.pgn.read_game(pgn)
+moves = first_game.main_line()
+node = first_game
+while not node.is_end():
+    next_node = node.variations[0]
+    print(board.fen())
+    board.push_san(node.board().san(next_node.move))
+    print(node.board().san(next_node.move))
+    node = next_node
